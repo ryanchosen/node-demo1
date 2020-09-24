d182ac1d932710d62bf3d805c8065e14508ad90e
RyanSu <563607356@qq.com>
2020-09-24 19:18:39 +0800

    First commit

---
+++ server.js
@@ -0,0 +1,59 @@
+var http = require('http')
+var fs = require('fs')
+var url = require('url')
+var port = process.argv[2]
+
+if(!port){
+  console.log('请指定端口号好不啦？\nnode server.js 8888 这样不会吗？')
+  process.exit(1)
+}
+
+var server = http.createServer(function(request, response){
+  var parsedUrl = url.parse(request.url, true)
+  var pathWithQuery = request.url 
+  var queryString = ''
+  if(pathWithQuery.indexOf('?') >= 0){ queryString = pathWithQuery.substring(pathWithQuery.indexOf('?')) }
+  var path = parsedUrl.pathname
+  var query = parsedUrl.query
+  var method = request.method
+
+  /******** 从这里开始看，上面不要看 ************/
+
+  console.log('有个傻子发请求过来啦！路径（带查询参数）为：' + pathWithQuery)
+  console.log(request.headers); //输出请求头
+  if(path === '/'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.setHeader('Ryan','GettingBetter') //设置响应头
+    //response.write 设置响应体的内容
+    response.write(`<!DOCTYPE html> 
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="/x">
+        <title>Document</title>
+    </head>
+    <body>
+        为什么字体会变成红色？打开network看一下，这里其实是两个请求，还请求了一个css文件
+    </body>
+    </html>`)
+    response.end()
+  } else if(path === '/x'){
+    response.statusCode = 200
+    response.setHeader('Content-Type', 'text/css;charset=utf-8')
+    response.write(`body{color: red;}`)
+    response.end()
+  }else {
+    response.statusCode = 404
+    response.setHeader('Content-Type', 'text/html;charset=utf-8')
+    response.write(`你输入的路径不存在对应的内容`)
+    response.end()
+  }
+
+  /******** 代码结束，下面不要看 ************/
+})
+
+server.listen(port)
+console.log('监听 ' + port + ' 成功\n请用在空中转体720度然后用电饭煲打开 http://localhost:' + port)
+
